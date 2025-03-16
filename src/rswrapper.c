58062ad69b93d100ea4b90d6898b18f05df00461
Andy Grundman <andy@hybridized.org>
2025-03-16 04:38:54 -0400

    NanoRS: support multi-arch build on Windows with runtime detection.

--- src/rswrapper.c
+++ src/rswrapper.c
@@ -39,12 +39,12 @@
 #define gemm DECORATE_FUNC(gemm, ISA_SUFFIX)
 #define invert_mat DECORATE_FUNC(invert_mat, ISA_SUFFIX)
 
-#if defined(__x86_64__) || defined(__i386__)
+#if defined(__x86_64__) || defined(__i386__) || defined(_MSC_VER)
 
   // Compile a variant for SSSE3
   #if defined(__clang__)
     #pragma clang attribute push(__attribute__((target("ssse3"))), apply_to = function)
-  #else
+  #elif __GNUC__
     #pragma GCC push_options
     #pragma GCC target("ssse3")
   #endif
@@ -55,14 +55,14 @@
   #undef ISA_SUFFIX
   #if defined(__clang__)
     #pragma clang attribute pop
-  #else
+  #elif __GNUC__
     #pragma GCC pop_options
   #endif
 
   // Compile a variant for AVX2
   #if defined(__clang__)
     #pragma clang attribute push(__attribute__((target("avx2"))), apply_to = function)
-  #else
+  #elif __GNUC__
     #pragma GCC push_options
     #pragma GCC target("avx2")
   #endif
@@ -73,14 +73,14 @@
   #undef ISA_SUFFIX
   #if defined(__clang__)
     #pragma clang attribute pop
-  #else
+  #elif __GNUC__
     #pragma GCC pop_options
   #endif
 
   // Compile a variant for AVX512BW
   #if defined(__clang__)
     #pragma clang attribute push(__attribute__((target("avx512f,avx512bw"))), apply_to = function)
-  #else
+  #elif __GNUC__
     #pragma GCC push_options
     #pragma GCC target("avx512f,avx512bw")
   #endif
@@ -91,13 +91,14 @@
   #undef ISA_SUFFIX
   #if defined(__clang__)
     #pragma clang attribute pop
-  #else
+  #elif __GNUC__
     #pragma GCC pop_options
   #endif
 
 #endif
 
 // Compile a default variant, this will be the NEON version if __aarch64__
+// and the SSE3 version when built with MSVC on Windows.
 #define ISA_SUFFIX _def
 #include "../nanors/deps/obl/autoshim.h"
 #include "../nanors/rs.c"
@@ -117,12 +118,68 @@ reed_solomon_release_t reed_solomon_release_fn;
 reed_solomon_encode_t reed_solomon_encode_fn;
 reed_solomon_decode_t reed_solomon_decode_fn;
 
+#if defined(_MSC_VER) && defined(_M_X64)
+  // https://learn.microsoft.com/en-us/cpp/intrinsics/cpuid-cpuidex?view=msvc-170
+
+  // The EBX/ECX registers indicate CPU feature flags using bits.
+  // SSSE3: bit 9 of ECX
+  // AVX2: bit 5 of EBX
+  // AVX512F: bit 16 of EBX
+  // AVX512BW: bit 30 of EBX
+
+  #include <intrin.h>
+
+  int _msc_check_ebx(int bit) {
+    int cpuInfo[4] = {0};
+    __cpuid(cpuInfo, 0);
+    int maxFunctionId = cpuInfo[0];
+    if (maxFunctionId >= 7) {
+      __cpuidex(cpuInfo, 7, 0);
+      return (cpuInfo[1] & (1 << bit)) != 0;
+    }
+    return 0;
+  }
+
+  int _msc_supports_avx2(void)     { return _msc_check_ebx(5); }
+  int _msc_supports_avx512f(void)  { return _msc_check_ebx(16); }
+  int _msc_supports_avx512bw(void) { return _msc_check_ebx(30); }
+
+  int _msc_supports_ssse3(void) {
+    int cpuInfo[4] = {0};
+    __cpuid(cpuInfo, 1);
+    return (cpuInfo[2] & (1 << 9)) != 0;
+  }
+#endif
+
 /**
  * @brief This initializes the RS function pointers to the best vectorized version available.
  * @details The streaming code will directly invoke these function pointers during encoding.
  */
 void reed_solomon_init(void) {
-#if defined(__x86_64__) || defined(__i386__)
+#if defined(_MSC_VER) && defined(_M_X64)
+  // Visual Studio
+  if (_msc_supports_avx512f() && _msc_supports_avx512bw()) {
+    reed_solomon_new_fn = reed_solomon_new_avx512;
+    reed_solomon_release_fn = reed_solomon_release_avx512;
+    reed_solomon_encode_fn = reed_solomon_encode_avx512;
+    reed_solomon_decode_fn = reed_solomon_decode_avx512;
+    reed_solomon_init_avx512();
+  } else if (_msc_supports_avx2()) {
+    reed_solomon_new_fn = reed_solomon_new_avx2;
+    reed_solomon_release_fn = reed_solomon_release_avx2;
+    reed_solomon_encode_fn = reed_solomon_encode_avx2;
+    reed_solomon_decode_fn = reed_solomon_decode_avx2;
+    reed_solomon_init_avx2();
+  } else if (_msc_supports_ssse3()) {
+    reed_solomon_new_fn = reed_solomon_new_ssse3;
+    reed_solomon_release_fn = reed_solomon_release_ssse3;
+    reed_solomon_encode_fn = reed_solomon_encode_ssse3;
+    reed_solomon_decode_fn = reed_solomon_decode_ssse3;
+    reed_solomon_init_ssse3();
+  } else
+
+#elif defined(__x86_64__) || defined(__i386__)
+  // gcc & clang
   if (__builtin_cpu_supports("avx512f") && __builtin_cpu_supports("avx512bw")) {
     reed_solomon_new_fn = reed_solomon_new_avx512;
     reed_solomon_release_fn = reed_solomon_release_avx512;
@@ -142,7 +199,9 @@ void reed_solomon_init(void) {
     reed_solomon_decode_fn = reed_solomon_decode_ssse3;
     reed_solomon_init_ssse3();
   } else
+
 #endif
+  //
   {
     reed_solomon_new_fn = reed_solomon_new_def;
     reed_solomon_release_fn = reed_solomon_release_def;
